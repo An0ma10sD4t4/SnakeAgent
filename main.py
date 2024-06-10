91033b15a89b67c05d9d57714b75d8a1d9800e84
An0ma10sD4t4 <an0ma10s.d4t4@gmail.com>
2024-06-09 20:08:07 -0400

    Animated Background Implementation

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import sys
 import os
 import ctypes
 import platform
+import subprocess
 import winreg as reg
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QPushButton, QComboBox, QVBoxLayout, QFileDialog, QMessageBox
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QPushButton, QVBoxLayout, QFileDialog, QMessageBox
+from PyQt5.QtGui import QPixmap, QMovie
 from dotenv import load_dotenv
 
 class WallpaperChanger( QMainWindow ):
@@ -23,25 +25,15 @@ class WallpaperChanger( QMainWindow ):
         self.setCentralWidget( central_widget )
 
         self.label = QLabel( 'No Image Selected', self )
-
+        # self.label.setAlignment( Qt.AlignCenter )
         self.button_select = QPushButton( 'Select Image', self )
         self.button_select.clicked.connect( self.select_image )
-
-        self.style_select = QComboBox()
-        self.style_select.addItem( 'Center' )
-        self.style_select.addItem( 'Tile' )
-        self.style_select.addItem( 'Stretch' )
-        self.style_select.addItem( 'Fit' )
-        self.style_select.addItem( 'Fill' )
-        self.style_select.addItem( 'Span' )
-
         self.button_set = QPushButton( 'Set Wallpaper', self )
         self.button_set.clicked.connect( self.set_wallpaper )
 
         layout = QVBoxLayout()
         layout.addWidget( self.label )
         layout.addWidget( self.button_select )
-        layout.addWidget( self.style_select )
         layout.addWidget( self.button_set )
 
         central_widget.setLayout( layout )
@@ -52,17 +44,29 @@ class WallpaperChanger( QMainWindow ):
             self, 
             'QFileDialog.getOpenFileName()', 
             '', 
-            'Images ( *.png *.xpm *.jpg *.jpeg *.bmp );;All files ( * )', 
+            'Images ( *.png *.xpm *.jpg *.jpeg *.bmp *.gif );;All files ( * )', 
             options=options 
         )
 
         if file_name:
-            display_selected_image = QPixmap( file_name )
-            self.label.setPixmap( display_selected_image.scaledToWidth( 500 ) )
+            if file_name.endswith == '.gif':
+                self.display_gif( gif=file_name )
+            else:
+                self.display_image( image=file_name )
 
             self.image_path = file_name
         else:
-            self.label.setText( 'No Image Selected' )
+            QMessageBox.warning( self, 'Warning', 'No Image Selected' )
+            return
+
+    def display_image( self, image ):
+        display_selected_image = QPixmap( image )
+        self.label.setPixmap( display_selected_image.scaledToWidth( 500 ) )
+
+    def display_gif( self, gif ):
+        gif_movie = QMovie( gif )
+        self.label.setMovie( gif_movie )
+        gif_movie.start()
 
     def set_wallpaper( self ):
         image_path = self.image_path
@@ -74,48 +78,36 @@ class WallpaperChanger( QMainWindow ):
             return
         
         os_name = platform.system()
+        if image_path.endswith == '.gif' and os_name == 'Windows':
+            pass
+        else:
+            if os_name == 'Windows':
+                self.set_static_wallpaper( image_path=image_path )
+            elif os_name == 'Darwin':
+                script = f"""osascript -e 'tell application "Finder" to set desktop picture to POSIX file "{image_path}"'"""
+                os.system( script )
+            elif os_name == "Linux":
+                desktop_env = os.getenv("DESKTOP_SESSION")
+                if desktop_env in ["gnome", "unity", "cinnamon", "mate"]:
+                    os.system(f"gsettings set org.gnome.desktop.background picture-uri file://{image_path}")
+                elif desktop_env == "xfce":
+                    os.system(f"xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s {image_path}")
+                elif desktop_env == "lxde":
+                    os.system(f"pcmanfm --set-wallpaper={image_path} --wallpaper-mode=stretch")
+                else:
+                    QMessageBox.warning(self, "Warning", "Your desktop environment is not supported!")
 
-        if os_name == 'Windows':
-            style_dict = {
-                'Center': '0',
-                'Tile': '1',
-                'Stretch': '2',
-                'Fit': '6',
-                'Fill': '10',
-                'Span': '22'
-            }
+    def set_gif_wallpaper( self, image_path ):
+        try:
+            subprocess.run( [ 'livelywpf', '--file', image_path ], check=True )
+        except Exception as error:
+            QMessageBox.critical( self, 'Error', f'Failed to Set Gif Wallpaper: { error }' )
 
-            selected_style = self.style_select.currentText()
-            if selected_style not in style_dict:
-                QMessageBox.warning( self, 'Warning', 'Style Currently Unsupported' )
-                return
-
-            try:
-                ctypes.windll.user32.SystemParametersInfoW( 20, 0, image_path, 0 )
-
-                key = reg.OpenKey(reg.HKEY_CURRENT_USER, r'Control Panel\Desktop', 0, reg.KEY_SET_VALUE)
-                reg.SetValueEx(key, 'WallpaperStyle', 0, reg.REG_SZ, style_dict[selected_style])
-                reg.SetValueEx(key, 'TileWallpaper', 0, reg.REG_SZ, '0' if selected_style != 'Tile' else '1')
-                key.Close()
-
-                ctypes.windll.user32.SystemParametersInfoW( 20, 0, image_path, 3 )
-            except Exception as e:
-                QMessageBox.critical( self, 'Error', f'Failed To Set Wallpaper: { e }' )
-                return
-        
-        elif os_name == 'Darwin':
-            script = f"""osascript -e 'tell application "Finder" to set desktop picture to POSIX file "{image_path}"'"""
-            os.system( script )
-        elif os_name == "Linux":
-            desktop_env = os.getenv("DESKTOP_SESSION")
-            if desktop_env in ["gnome", "unity", "cinnamon", "mate"]:
-                os.system(f"gsettings set org.gnome.desktop.background picture-uri file://{image_path}")
-            elif desktop_env == "xfce":
-                os.system(f"xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s {image_path}")
-            elif desktop_env == "lxde":
-                os.system(f"pcmanfm --set-wallpaper={image_path} --wallpaper-mode=stretch")
-            else:
-                QMessageBox.warning(self, "Warning", "Your desktop environment is not supported!")
+    def set_static_wallpaper( self, image_path ):
+        try:
+            ctypes.windll.user32.SystemParametersInfoW( 20, 0, image_path, 0 )
+        except Exception as error:
+            QMessageBox.critical( self, 'Error', f'Failded to Set Static Wallpaper: { error }' )
 
 if __name__ == '__main__':
     app = QApplication( sys.argv )
